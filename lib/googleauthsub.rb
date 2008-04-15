78c6c1e132db0db15a9ae1d580dec662c89abd31
Stuart Coyle <stuart.coyle@gmail.com>
2008-04-16 07:43:00 +1000

    Tidied up and added some comments.

--- lib/googleauthsub.rb
+++ lib/googleauthsub.rb
@@ -1,6 +1,6 @@
-# AuthSub - Ruby on Rails plugin for Google Authorization
+# AuthSub - Ruby library for Google Authorization
 # # Copyright 2008 Stuart Coyle <stuart.coyle@gmail.com>
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
 # "Software"), to deal in the Software without restriction, including
@@ -8,10 +8,10 @@
 # distribute, sublicense, and/or sell copies of the Software, and to
 # permit persons to whom the Software is furnished to do so, subject to
 # the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -25,7 +25,8 @@ require 'net/https'
 require 'openssl'
 require 'base64'
 
-
+# Note: The module declared here may change depending on what other developers are using
+#
 module GData
     GOOGLE_HOST_URL = "www.google.com"
     GOOGLE_AUTHSUB_BASE_PATH = "/accounts"
@@ -45,17 +46,17 @@ end
 
 
 # GoogleAuthSub
-# This class handles the Google Authentication for Web Applications API 
-# 
+# This class handles the Google Authentication for Web Applications API
+#
 class GoogleAuthSub
- 
+
   attr_accessor :target, :scope, :session, :secure, :next_url, :token, :sigalg
-  
-  # key can be a File, String or OpenSSL::Pkey::RSA
-  # Sets the private key to use for secure sessions. 
-  # This should corespond to the public key sent to Google in 
-  # the registration process. 
-  # For registration details see: 
+
+  # +key+ can be a File, String or OpenSSL::Pkey::RSA
+  # Sets the private key to use for secure sessions.
+  # This should correspond to the public key sent to Google in
+  # the registration process.
+  # For registration details see:
   #   http://code.google.com/apis/accounts/docs/RegistrationForWebAppsAuto.html
   #
   # This sets the class variable @@pkey to an OpenSSL::Pkey::RSA object
@@ -73,70 +74,70 @@ class GoogleAuthSub
          raise AuthSubError, "Private Key in wrong format. Require IO, String or OpenSSL::Pkey::RSA, got #{key.class}"
      end
   end
-  
+
   # Create a new GoogleAuthsub object
   # Options specified in +opts+ consist of:
   #
   # * :next_url - (String)  The url to redirect back to once the user has signed in to Google.
   # * :scope_url - (String) The service from Google that you wish to receive data from with this token.
-  # * :session - (boolean)  Wether the token is able to be used to get a session token or is just one use.
-  # * :secure - (boolean) Wether the token can be used for sessions.
-  #
+  # * :session - (boolean)  Whether the token is able to be used to get a session token or is just one use.
+  # * :secure - (boolean) Whether the token can be used for sessions.
+  # * :sigalg - (String) Currently not needed, as the Authsub specification only has rsa-sha1.  
   def initialize(opts = {})
     self.next_url = opts[:next_url] || ''
     self.scope = opts[:scope_url] || ''
     self.session = opts[:session] || false
     self.secure = opts[:secure] || false
-    self.sigalg = "rsa-sha1"
+    self.sigalg = opts[:sigalg] || "rsa-sha1"
   end
-  
+
   # This returns a URI::HTTPS object which contains the Google url to request a token from.
   def request_url
      raise AuthSubError, "Invalid next URL: #{@next_url}" if !full_url?(@next_url)
      raise AuthSubError, "Invalid scope URL: #{@scope}" if !full_url?(@scope)
-     
+
      query = "next=" << @next_url << "&scope=" << @scope << "&session="<<
              (session_token? ? '1' : '0')<< "&secure="<< (secure_token? ? '1' : '0')
      query = URI.encode(query)
      URI::HTTPS.build({:host => GOOGLE_HOST_URL, :path => GOOGLE_AUTHSUB_REQUEST_PATH, :query => query })
   end
-  
+
   # +url+ :the URL received from Google once the user has signed in.
   #
   # This method extracts the token from the request url that Google
-  # sends the user back to. 
+  # sends the user back to.
   # This url will be like: http://www.example.com/next?Token=CMDshfjfkeodf
-  # In Rails you don't need this method, just use 
+  # In Rails applications you don't need this method, just use
   # +GoogleAuthsub#token=params[:token]+
-  # 
+  #
   def receive_token(url)
     q = url.query.match(/Token=(.*)/)
     @token = q[1] if !q.nil?
   end
-  
+
   # Returns true if this token can be exchanged for a session token
   def session_token?
     session == true
   end
-  
+
   # Returns true if the token is used for secure sessions
   def secure_token?
     secure == true
   end
-  
+
   # session_token
   # This method exchanges a previously received single use token with a session token.
   # Raises error if an invalid response is received.
   def session_token
     url =  URI::HTTPS.build({:host => GOOGLE_HOST_URL,
-        :path => GOOGLE_AUTHSUB_SESSION_TOKEN_PATH})   
+        :path => GOOGLE_AUTHSUB_SESSION_TOKEN_PATH})
     begin
      @token = get(url).body.match(/^Token=(.*)$/)[1]
    rescue
      raise AuthSubError, "Invalid session token response."
    end
   end
-  
+
   # revoke_token
   # This revokes either a single use or session token
   # The token will not be able to be used again if this call is successful.
@@ -151,10 +152,10 @@ class GoogleAuthSub
      false
     end
   end
-  
+
   # token_info
-  # Returns the information for the session token
-  # as a map {:target, :scope, :secure} 
+  # Returns the information for the session token from Google.
+  # Returns a map {:target, :scope, :secure}
   def token_info
     url =  URI::HTTPS.build({:host=>GOOGLE_HOST_URL,
       :path => GOOGLE_AUTHSUB_TOKEN_INFO_PATH})
@@ -167,10 +168,10 @@ class GoogleAuthSub
     rescue
       raise AuthSubError, "Google Authsub Error: invalid token info packet received."
     end
-   
+
     return info
   end
-  
+
   # get +url+
   # Does a HTTP GET request to Google using the AuthSub token.
   # This returns a Net::HTTPResponse object.
@@ -183,28 +184,28 @@ class GoogleAuthSub
   def post(url)
      authsub_http_request(Net::HTTP::Post,url)
   end
+
   # put +url+
   # Does a HTTP PUT request to Google using the AuthSub token.
-  # This returns a Net::HTTPResponse object. 
+  # This returns a Net::HTTPResponse object.
   def put(url)
     authsub_http_request(Net::HTTP::Put,url)
-  end 
-  
+  end
+
   # delete +url+
   # Does a HTTP DELETE request to Google using the AuthSub token.
   # This returns a Net::HTTPResponse object.
   def delete(url)
     authsub_http_request(Net::HTTP::Delete,url)
   end
-  
+
   private
- 
-  
+
   def authsub_http_request(method, u) #:nodoc:
     case u
-      when String 
+      when String
         # Add scope
-        u = (@scope << u) if @scope && !u.include?(@scope)        
+        u = (@scope << u) if @scope && !u.include?(@scope)
         begin
           url = URI.parse(u)
         rescue URI::InvalidURIError
@@ -212,10 +213,10 @@ class GoogleAuthSub
         end
       when URI
         url = u
-      else 
+      else
         raise AuthSubError, "url must be String or URI, #{url.class} received."
-    end 
-    
+    end
+
     if method.superclass != Net::HTTPRequest
       raise AuthSubError, "method must be a Net::HTTPRequest subclass. #{method} received."
     end
@@ -233,8 +234,8 @@ class GoogleAuthSub
     response
   end
 
-  # FIXME SIGN DATA NOT TOKEN!!
-  def authorization_header(request, url) 
+  # Construct the authorization header for a request
+  def authorization_header(request, url)
     case secure_token?
     when false
       return "AuthSub token=\"#{@token}\""
@@ -251,24 +252,24 @@ class GoogleAuthSub
     nonce = OpenSSL::BN.rand_range(2**64)
     data = request.method + ' ' + url.to_s + ' ' + Time.now.to_i.to_s + ' ' + nonce.to_s
   end
-  
+
   def sign_data(data)
      Base64.b64encode(@@pkey.sign(OpenSSL::Digest::SHA1.new, data))
    end
 
-  # Checks wether a URL is a full url, i.e. has scheme, host and path.
+  # Checks whether a URL is a full url, i.e. has all of scheme, host and path.
   def full_url?(url)
     # First check if it is a bad uri
     begin
       u = URI.parse(url)
-    rescue URI.InvalidURIError 
+    rescue URI.InvalidURIError
       return false
     end
-  
+
     return false if u.scheme.nil? || u.host.nil? || u.path.nil?
     true
   end
-  
+
 end
-  
+
 end
